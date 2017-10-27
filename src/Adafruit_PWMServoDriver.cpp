36b8b5e16297de5aebe4d68a06efdf7c134b91ab
djzxz <31861559+djzxz@users.noreply.github.com>
2017-10-27 22:25:08 +0200

    vcsad

--- src/Adafruit_PWMServoDriver.cpp
+++ src/Adafruit_PWMServoDriver.cpp
@@ -42,14 +42,14 @@ void Adafruit_PWMServoDriver::reset(void) {
  write8(PCA9685_MODE1, 0x0);
 }
 
-void Adafruit_PWMServoDriver::setPWMFreq(float freq) {
+void Adafruit_PWMServoDriver::setPWMFreq(int freq) {
   //Serial.print("Attempting to set freq ");
   //Serial.println(freq);
-  //float buff = freq * 0.9;
-  freq *= 0.9;  // Correct for overshoot in the frequency setting (see issue #11).
+  float buff = freq * 0.9;
+  //(float)freq *= 0.9;  // Correct for overshoot in the frequency setting (see issue #11).
   float prescaleval = 25000000;
   prescaleval /= 4096;
-  prescaleval /= freq;
+  prescaleval /= buff;
   prescaleval -= 1;
   if (ENABLE_DEBUG_OUTPUT) {
     //Serial.print("Estimated pre-scale: "); Serial.println(prescaleval);
